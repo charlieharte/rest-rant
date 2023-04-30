036f923f5746c81cd16d4523049301e9b5149a49
Charlie Harte <charlieharte@Charlies-MBP.attlocal.net>
2023-04-30 18:07:34 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/second', (req,res) => {
 })
 
 app.get('*', (req,res) => {
-  res.status(404).send('<h1>404 Page</h1>')
+  res.status(404).render('error404')
 })
 
 // const PORT = process.env.PORT || 3000
